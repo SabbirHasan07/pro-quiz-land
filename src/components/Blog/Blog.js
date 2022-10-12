3ff89a14387b7cbb428e9a841b3302d44d8b2475
reveals007 <sabbirhasanleom10@gmail.com>
2022-10-12 14:12:03 +0600

    commit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -12,7 +12,8 @@ const Blog = () => {
             <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
            </div>
            <div className="blog">
-            <h1>Use of ref.</h1>
+            <h1>Question- 03: Use of ref.</h1>
+            <p>The ref keyword indicates that a variable is a reference, or an alias for another object. It's used in five different contexts: In a method signature and in a method call, to pass an argument to a method by reference. For more information, see Passing an argument by reference.</p>
            </div>
         </div>
     );
