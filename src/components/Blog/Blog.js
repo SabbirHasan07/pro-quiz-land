300fa8a1adff3681313cdc6b0e13e62ae80157ad
reveals007 <sabbirhasanleom10@gmail.com>
2022-10-12 14:10:41 +0600

    commit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,10 +8,12 @@ const Blog = () => {
             <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
            </div>
            <div className="blog">
-            <h1>how does contex api works?</h1>
+            <h1>Question- 02: how does contex api works?</h1>
             <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
            </div>
-           <div className="blog"></div>
+           <div className="blog">
+            <h1>Use of ref.</h1>
+           </div>
         </div>
     );
 };
