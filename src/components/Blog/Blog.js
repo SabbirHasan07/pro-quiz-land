344bb4784e36c8fe1271ff4b9fc6c28a22bdfd03
reveals007 <sabbirhasanleom10@gmail.com>
2022-10-12 14:08:30 +0600

    commit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,7 +7,9 @@ const Blog = () => {
             <h1> Question- 01: what is the purpose of react router?</h1>
             <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
            </div>
-           <div className="blog"></div>
+           <div className="blog">
+            <h1>how does contex api works?</h1>
+           </div>
            <div className="blog"></div>
         </div>
     );
