2f6002c1e8ae38c724a4699fbf4125b21551631f
reveals007 <sabbirhasanleom10@gmail.com>
2022-10-12 14:09:03 +0600

    commit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -9,6 +9,7 @@ const Blog = () => {
            </div>
            <div className="blog">
             <h1>how does contex api works?</h1>
+            <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
            </div>
            <div className="blog"></div>
         </div>
